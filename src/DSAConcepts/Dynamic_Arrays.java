2743113acaf8cefbf1d4f31f907e4453e678c5af
NISHCHITHA G <nishchithahegde777@gmail.com>
2025-08-16 20:53:14 +0530

    commit

---
+++ src/DSAConcepts/Dynamic_Arrays.java
@@ -0,0 +1,17 @@
+package DSAConcepts;
+import java.util.Arrays;
+public class Dynamic_Arrays {
+    public static void main(String args[]){
+        int[][] arr = {
+                {1,2,3},
+                {5,6},
+                {7,8}
+        };
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
